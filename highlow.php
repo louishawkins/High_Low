e5fa46dce1182b0453367f35e0e7d8b2349dd09c
Louis Hawkins <louishawkins@gmail.com>
2014-10-03 11:07:27 -0500

    Working, added cheat mode

--- highlow.php
+++ highlow.php
@@ -1,14 +1,15 @@
 <?php
 
-define("NUMBER", mt_rand(0,1000));
+$_mode = $argv[1];
 
+define("NUMBER", mt_rand(0,1000));
 echo "Welcome to the number game!\n";
 fwrite(STDOUT, "Guess which number I'm thinking of (between 0 and 1000)!\n");
-echo NUMBER;
+
+if($_mode == "-c"){echo "(It's " . NUMBER . ".)\n";}
 
 while ($guess != NUMBER) {
 $guess = fgets(STDIN);
-
 
 	if ($guess == NUMBER) {
 		fwrite(STDOUT, "That is the correct number!\n+ 10000 points!\n");
