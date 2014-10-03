ebf93dbb693228ea3ab36f68fb058edab8ccc9a0
Louis Hawkins <louishawkins@gmail.com>
2014-10-03 14:33:20 -0500

    Added help, solved _max less than problem

--- highlow.php
+++ highlow.php
@@ -12,6 +12,9 @@ if ($_mode == "-n") {
 		$_max = 1000;
 		define("NUMBER", mt_rand(0,1000));
 	}
+	elseif ($_max < $_min) {
+		define("NUMBER", mt_rand($_max,$_min));
+	}
 	else {
 		define("NUMBER", mt_rand($_min, $_max));
 	}
@@ -23,11 +26,12 @@ else {
 }
 
 
-
 echo "Welcome to the number game!\n";
 echo "Guess which number I'm thinking of (between {$_min} and {$_max})!\n";
 
 if($_mode == "-c"){echo "(It's " . NUMBER . ".)\n";}
+
+elseif($_mode == "-h"){echo "-n [number1] [number2] -- will start with lower value and end with higher value.\n-c Cheat mode; shows number genereated by computer.";}
 
 else {
 	while ($guess != NUMBER) {
