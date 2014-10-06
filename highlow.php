a0187e81af0c35377ff5227010041873652841a2
Louis Hawkins <louishawkins@gmail.com>
2014-10-06 15:14:55 -0500

    Full vesion update 2.0

--- highlow.php
+++ highlow.php
@@ -1,14 +1,18 @@
 <?php
 
+$_score = 0;
+$_scorePossible = 0;
+
+do {
+
 $_mode = $argv[1];
 $_min = $argv[2];
 $_max = $argv[3];
-$points = 0;
+//$_score = 0;
 
 if ($_mode == "-n") {
 
-	if($argv[2] == false || $argv[3] == false){
-	    $_mode = "normal";
+	if($argc < 2 || is_numeric($_min) == false || is_numeric($_max) == false){
 		$_min = 0;
 		$_max = 1000;
 		define("NUMBER", mt_rand(0,1000));
@@ -26,28 +30,42 @@ else {
 		define("NUMBER", mt_rand(0,1000));	
 }
 
+echo `clear`;
+echo "*******************************************************************\n";
+echo "GUESS THE NUMBER VERSION 2.0\n(c)1985\n";
+echo "*******************************************************************\n\n";
+echo "SCORE: {$_score}/{$_scorePossible}\n";
 
-echo "Welcome to the number game!\n";
-echo "Guess which number I'm thinking of (between {$_min} and {$_max})!\n";
+if($_scorePossible != 0){
+	echo "ACCURACY: " . ($_score / $_scorePossible) * 100 . "%\n\n";
+}
+else {echo "ACCURACY: 0%\n\n";}
 
-if($_mode == "-c"){echo "(It's " . NUMBER . ".)\n";}
+echo "Guess which number I'm thinking of (between {$_min} and {$_max})!\n\n";
 
-elseif($_mode == "-h"){echo "-n [number1] [number2]; will start with lower value and end with higher value.\n-c Cheat; Enter in command line or during game. Shows number genereated by computer and exits game.\n\n";}
+if ($_mode == "-h"){echo "-n [number1] [number2]; will start with lower value and end with higher value.\n-c Cheat; Shows number genereated by computer and exits game.\n\n";}
 
 else {
 		$points = 1010;
 
 		do {
-
+  			echo "> ";
 			$guess = trim(fgets(STDIN));
 
 			if($guess == (string) '-c') {
 				echo "(It's " . NUMBER . ".)\n";
 				$guess = NUMBER;
+				$_play = false;
+
 			}
 
 			elseif ($guess == NUMBER) {
 				fwrite(STDOUT, "That is the correct number!\n+ {$points} points!\n");
+
+				echo "Play again?\n> ";
+				$playAgain = trim(strtolower(fgets(STDIN)));
+				if ($playAgain == 'yes' || $playAgain == 'y') {$_play = true;}
+				else{$_play = false;}
 
 			}
 
@@ -71,8 +89,17 @@ else {
 				}
 			}
 			$points = $points - 10;
+	
+			echo "\n";
+
+			
 		} while ($guess != NUMBER);
-};
+}
+
+		$_score = $_score + $points + 10;
+		$_scorePossible += 1000;
+
+} while ($_play == true);
 
 exit(0);
 
